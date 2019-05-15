ce61db1618aab35b0c48ac93208224a6fa311647
Yixiong <yixiongd@student.unimelb.edu.au>
2019-05-15 16:54:37 +1000

    Update

---
+++ Parameters.java
@@ -0,0 +1,35 @@
+
+public class Parameters {
+	
+	public static float ALBEDO_WHITE;
+	
+	public static float ALBEDO_BLACK;
+	
+	public static float ALBEO_GROUND;
+	
+	public static float LUMINOSITY;
+	
+	public static float DIFFSION_RATE;
+	
+	public static int SENCERIO;
+	
+	public static int DAISY_LIFE_EXPECTANCY;
+	
+	public static float START_WHITE;
+	
+	public static float START_BLACK;
+	
+	public static int MIN_PRODUCE_TEMP;
+	
+	public static int MAX_PRODUCE_TEMP;
+	
+	public static int START_TEMP;
+	
+	public static int WORLD_SIZE_X;
+	
+	public static int WORLD_SIZE_Y;
+	
+	public static int ROUNDS;
+	
+
+}
